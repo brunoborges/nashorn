3d1ebfe0b5f59b48ba9d6e8ae73955ab8420cb0a
Bruno Borges <bruno.borges@oracle.com>
2014-08-28 15:45:45 -0300

    simple base64 encoder script

---
+++ util/base64.js
@@ -0,0 +1,13 @@
+/*
+ * Author: Bruno Borges
+ * Twitter: @brunoborges
+ */
+if (arguments.length === 0 || arguments[0] === '-h') {
+   print('Usage:');
+   print(' $> jjs base64.js -- text');
+   exit(1);
+}
+
+var Base64 = Packages.java.util.Base64;
+var String = Packages.java.lang.String;
+print('Base64: ' + new String(Base64.getEncoder().encode(arguments[0].getBytes()), "UTF-8"));
