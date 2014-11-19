f64929b99e85b2f9dc3a60508349bd114414da3f
Bruno Borges <bruno.borges@oracle.com>
2014-11-19 19:48:56 -0200

    smallest version

--- browser/browser.js
+++ browser/browser.js
@@ -1,24 +1,6 @@
-/*
- * Author: Bruno Borges
- * Twitter: @brunoborges
- */
-if (arguments.length === 0 || arguments[0] === '-h') {
-   print('Usage:');
-   print(' $> jjs -fx browser.js -- http://www.google.com');
-   exit(1);
-}
-
-var WebView   = javafx.scene.web.WebView;
-var StackPane = javafx.scene.layout.StackPane;
-var Scene     = javafx.scene.Scene;
-var url       = arguments[0];
-function start(stage) {
-    var browser = new WebView();
-    browser.getEngine().load(url);
-    var root = new StackPane();
-    root.children.add(browser);
-    stage.scene = new Scene(root, 1080, 800);
-    stage.title = "browser.js";
-    stage.show();
-}
-
+#!/usr/bin/jjs -fx -scripting
+load("fx:web.js")
+var wv = new WebView()
+wv.engine.load("http://java.com")
+$STAGE.scene = new Scene(wv, 800, 600)
+$STAGE.show()
